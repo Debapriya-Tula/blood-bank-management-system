31123ae92cf46ce9d3fb81b5557603df13f3563e
Debapriya-Tula <tdebapriya@gmail.com>
2018-10-09 00:22:17 +0530

    Created Register Model(structure),templates and static folder added

---
+++ BBMS/registration/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Register(models.Model):
+    first_name = models.CharField(max_length = 200)
+    last_name = models.CharField(max_length = 200)
+    user_name = models.CharField(max_length = 100,unique = True)
+    password = models.CharField(max_length = 20)
+    blood_group = models.CharField(max_length = 3)
+
+    def __str__(self):
+        return self.user_name
