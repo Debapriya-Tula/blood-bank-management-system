26ff822259f23c55907d74e254bd19e755cce7e3
sowri1 <32513895+sowri1@users.noreply.github.com>
2018-10-13 16:17:15 +0530

    Update views.py

--- BBMS/registration/views.py
+++ BBMS/registration/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-from registration.models import Register
-# Create your views here.
+def register(request):
+	if request.method == 'POST':
+		name = request.POST.get('name')
+		uname = request.POST.get('uname')
+		email = request.POST.get('email')
+		passwd = request.POST.get('passwd')
+		dob = request.POST.get('dob')
+		line1 = request.POST.get('line1')
+		line2 = request.POST.get('line2')
+		city = request.POST.get('city')
+		postalcode = request.POST.get('postalcode')
+		country = request.POST.get('country')
+		context = {
+            'name': name,
+            'email': email,
+            'uname': uname,
+            'dob':dob,
+            'line1':line1,
+            'line2':line2,
+            'city':city,
+            'postalcode':postalcode,
+            'country':country,
+        }
+		template = loader.get_template('confirm_register.html')
+		return HttpResponse(template.render(contect,request))
+	else:
+		template = loader.get_template('register.html')
+		return HttpResponse(template.render())
 
 def index(request):
-    return HttpResponse("Bingo!")
+	template = loader.get_template('index.html')
+	return HttpResponse(template.render())
