e456f6e753882adba11c86fa9c0a372a98ca69a9
Debapriya-Tula <tdebapriya@gmail.com>
2018-10-13 12:09:26 +0530

    Created the project directory, registration app

--- BBMS/registration/models.py
+++ BBMS/registration/models.py
@@ -6,6 +6,7 @@ class Register(models.Model):
     first_name = models.CharField(max_length = 200)
     last_name = models.CharField(max_length = 200)
     user_name = models.CharField(max_length = 100,unique = True)
+    email = models.EmailField(max_length = 200,unique = True)
     password = models.CharField(max_length = 20)
     blood_group = models.CharField(max_length = 3)
 
