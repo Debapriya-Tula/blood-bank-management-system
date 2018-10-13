03ab86953476b77dc159bc9ef611f0dbe2b56e09
sowri1 <32513895+sowri1@users.noreply.github.com>
2018-10-13 16:16:33 +0530

    updated models

--- BBMS/registration/models.py
+++ BBMS/registration/models.py
@@ -1,14 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class donors(models.Model):
+	id = models.IntegerField(primary_key=True)
+	name = models.CharField(max_length=150,null=True)
+	email = models.CharField(max_length=50)
+	uname = models.CharField(max_length=50)
+	passwd = models.CharField(max_length=30)
+	dob = models.DateField()
+	line1 = models.CharField(max_length=150)
+	line2 = models.CharField(max_length=150)
+	postalcode = models.CharField(max_length=6)
+	city = models.CharField(max_length=150)
+	country = models.CharField(max_length=150)
 
-class Register(models.Model):
-    first_name = models.CharField(max_length = 200)
-    last_name = models.CharField(max_length = 200)
-    user_name = models.CharField(max_length = 100,unique = True)
-    email = models.EmailField(max_length = 200,unique = True)
-    password = models.CharField(max_length = 20)
-    blood_group = models.CharField(max_length = 3)
+class hospital(models.Model):
+	id = models.IntegerField(primary_key=True)
+	name = models.CharField(max_length=150,null=True)
+	email = models.CharField(max_length=50)
+	uname = models.CharField(max_length=50)
+	passwd = models.CharField(max_length=30)
+	license = models.CharField(max_length=30)
+	l_exp = models.DateField()
+	line1 = models.CharField(max_length=150)
+	line2 = models.CharField(max_length=150)
+	postalcode = models.CharField(max_length=6)
+	city = models.CharField(max_length=150)
+	country = models.CharField(max_length=150)
 
-    def __str__(self):
-        return self.user_name
+class users(models.Model):
+	id = models.IntegerField(primary_key=True)
+	name = models.CharField(max_length=150,null=True)
+	email = models.CharField(max_length=50)
+	uname = models.CharField(max_length=50)
+	passwd = models.CharField(max_length=30)
+	dob = models.DateField()
